728f48e9249255794e9ba92c70309bc5271654f1
raulescobar-g <raulescobar_g@tamu.edu>
2021-12-14 21:44:58 -0600

    can get multiline comments single line comments and associates comment by urgency

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,53 @@
 mod packet;
 #[macro_use] extern crate prettytable;
-use crate::packet::Packet;
+use crate::packet::*;
 use std::fs::*;
-use std::io::{BufReader, BufRead, Error,ErrorKind};
+use std::io::{Error, ErrorKind};
 use std::fs;
+use std::collections::LinkedList;
 use std::time::{Instant};
 use prettytable::{Table};
 use colored::*;
-
-fn iter_dir(path: String) ->Result<(i32, Vec<Packet>),Error> {
-    let files = if let Ok(open_file) = fs::read_dir(&path) {open_file} else {return Err(Error::new(ErrorKind::Other, format!("Could not read the directory: {}",path)));};
-
-    let mut todos:Vec<Packet> = Vec::new();
-    let mut filecount = 0;
-
-    for file in files {
-        if let Ok(ref this_file) = file {
-            if !this_file.path().is_dir() {
-                filecount += 1;
-
-                // panics when it gets here because it cannot open the file, make it optional to just skip this error
-                let data = if let Ok(open_file) = File::open(this_file.path()){open_file} else {return Err(Error::new(ErrorKind::Other, format!("Could not open file: {}",this_file.path().as_os_str().to_str().unwrap())))};
-                let buff = BufReader::new(data);
-                let mut line_number = 0;
-
-                let pathy = this_file.path();
-
-                let comment_pattern: &str = if let Some(ext) = pathy.extension(){ 
-                    let ans = match ext.to_os_string().to_str() {
-                        Some("rs")| Some("cpp")| Some("js")| Some("h")|Some("java") | Some("ts") => {"//"},
-                        Some("py") => {"#"},
-                        None => {
-                            filecount -= 1;
-                            continue;
-                        },
-                        _ => {
-                            filecount-= 1;
-                            continue;
-                        },
-                    };
-                    ans
-                }
-                else {
-                    filecount -= 1;
-                    continue;
-                };
-
-                for line in buff.lines() {
-                    line_number +=1;
-                    if let Ok(ref this_line) = line {                    
-                        if this_line.to_uppercase().trim().starts_with(comment_pattern) {
-                            let pack = Packet::new(this_line.trim().trim_start_matches( comment_pattern ).trim().to_string(), this_file.path().into_os_string().into_string().unwrap(), line_number);
-                            todos.push(pack);
-                        }
-                    }
-                    else {
-                        filecount -= 1;
-                        continue;
-                    }
-
-                }  
-            }
-            else {
-                let (f, mut iter_res) = if let Ok((num,packs)) = iter_dir(this_file.path().into_os_string().into_string().unwrap()) {
-                    (num, packs)
-                }
-                else {
-                    println!("{}{}","Could not open file/directory: ".red().bold(),this_file.file_name().as_os_str().to_str().unwrap());
-                    continue;
-                };
-                todos.append(&mut iter_res);
-                filecount += f;
-            }
-        }
-        else {
-            return Err(Error::new(ErrorKind::Other, "No directory entries."));
-        }
-    }
-    return Ok((filecount,todos));
-}
-
-//TODO: refactor iter_dir function its getting kinda big and should seperate concerns better
+use indicatif::{ProgressBar, ProgressStyle};
  
 //TODO: add a function that gets parameters from a config file
 //TODO: add a function that gets files to ignore maybe from the gitignore
 //TODO: finish integrating the packet class still updating class 
 //TODO: maybe pring everything in like a tree pattern that mirrors the project structure
 
-//TODO: add comment patterns for multi types of code files => multiline codes and bodies of text
-//TODO: add regex to handle different todo comment formats
-//TODO: add FIXME, and HACK as like an urgency system, perhaps add this to the packet class as enum
-
 //TODO: package app somehow
 //TODO: deploy to homebrew
 //TODO: cache results on a file but print the actual stuff on the terminal
 
-
-
 fn main() -> Result<(), Error>{
 
-    //get config options 
-
-    //start timer and start file crawl
-
-    let start = Instant::now();
-
-    let (files_traversed, todos): (i32, Vec<Packet>) = iter_dir(".".to_string()).expect(&format!("{}","Expecting Vector of data packets: ".red()));
+    //TODO:get config options 
+    // print to terminal or file
+    // gitignore file or similar
+    // custom urgency type???
 
     let outputfile = "./TodoPolice.txt";
-    File::create(outputfile).expect(&format!("{} {}", "Something went creating output file: ".red().bold(),outputfile));
+    
+    let size_of_dir = if let Ok(sz) = get_size(std::path::Path::new(".")){
+        sz
+    }
+    else {
+        println!("{}", "Could not get size of directory".red().bold());
+        1000000
+    };
 
+    let pb = ProgressBar::new(size_of_dir);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{spinner:.green} [{elapsed_precise}] [{wide_bar:green}] {bytes}/{total_bytes} ({eta})\n {msg}")
+        .progress_chars("=> "));
+
+    println!("{}","Searching for todos...".bold());
+
+    let (files_traversed,files_openned, todos): (i32,i32, LinkedList<Packet>) = iter_dir(".".to_string(), &pb).expect(&format!("{}","Expecting Vector of data packets: ".red()));
+
+    pb.finish_with_message(format!("✨ Done crawling {}/{} valid files in {} s",files_openned.to_string().green().bold(), files_traversed.to_string().green().bold(), pb.elapsed().as_secs_f32().to_string().green().bold()));
+
+    File::create(outputfile).expect(&format!("{} {}", "Something went creating output file: ".red().bold(),outputfile));
     
     let mut tables = Table::new();
     tables.add_row(row![bFb->"TODO",bFw->"PATH",bFg->"LINE#"]);
@@ -117,14 +55,7 @@ fn main() -> Result<(), Error>{
         tables.add_row(row![bFb->(*task).task,bFw->(*task).path ,bFg->(*task).line_n]);
     }
     fs::write(outputfile, &tables.to_string() ).expect(&format!("{} {}","Error writing to file:".red().bold(),outputfile));
-    tables.printstd();
-
-    let duration = start.elapsed();
-    //end of crawl and timer stop
-
-    //print time taken for single threaded crawl
-    println!("Files traversed: {} files",files_traversed.to_string().purple().magenta().bold());
-    println!("Time elapsed:    {} ms",duration.as_millis().to_string().purple().magenta().bold());
+    //tables.printstd();
 
 
     Ok(())
--- src/packet.rs
+++ src/packet.rs
@@ -1,20 +1,150 @@
+use std::fs::*;
+use std::fs;
+use std::path::Path;
+use std::collections::LinkedList;
+use std::{thread, time::Duration};
+use std::io::{BufReader, BufRead, Error,ErrorKind};
+use colored::*;
+use indicatif::{ProgressBar, ProgressStyle};
+use comment_parser::{self, SyntaxRule, get_syntax_from_path,CommentParser};
 
 
-
+pub enum Urgency {
+    TODO,
+    FIXME,
+    HACK,
+    XXX,
+}
 pub struct Packet {
     pub task: String,
     pub path : String,
     pub line_n : i32,
+    pub urgency: Urgency,
 }
 
 impl Packet {
-    pub fn new(task: String, path: String,line_n: i32) -> Self {
+    pub fn new(task: String, path: String,line_n: i32, urgency: Urgency) -> Self {
         Packet {
-            task:task, 
-            path: path,
-            line_n: line_n,
+            task,
+            path,
+            line_n,
+            urgency,
         }
     }
 
     
+}
+
+pub fn get_size<P>(path: P) -> std::io::Result<u64> where P: AsRef<Path>,{
+    let mut result = 0;
+
+    if path.as_ref().is_dir() {
+        for entry in read_dir(&path)? {
+            let _path = entry?.path();
+            if _path.is_file() {
+                result += _path.metadata()?.len();
+            } else {
+                result += get_size(_path)?;
+            }
+        }
+    } else {
+        result = path.as_ref().metadata()?.len();
+    }
+    Ok(result)
+}
+
+
+fn matcher(filename: &DirEntry, comment_rules: &[SyntaxRule], todos: &mut LinkedList<Packet>) -> Result<(),Error> {
+    
+    if let Ok(entire_file) = read_to_string(filename.path()){
+        let parser = CommentParser::new(entire_file.as_str(), comment_rules);
+
+        for comments in parser {
+            if let Some(_urgency) = comments.text().to_string().split_once(":").map(|s| (s.0.trim(),s.1.trim())){
+                let urgency = match _urgency.0 {
+                    "TODO" => {Urgency::TODO},
+                    "FIXME" => {Urgency::FIXME},
+                    "HACK" => {Urgency::HACK},
+                    "XXX" => {Urgency::XXX},
+                    _ => {continue;},
+                };
+
+                let pack = Packet::new(_urgency.1.to_string(), filename.path().into_os_string().into_string().unwrap(), 0, urgency);
+                todos.push_back(pack);
+            }
+                
+        }
+            
+        
+    }            
+    else {
+        return Err(Error::new(ErrorKind::Other, "File is not utf-8, or could not read lines for some reason."));
+    }
+
+    
+
+    return Ok(());
+}
+
+fn get_todos(filename: &DirEntry) -> Option<LinkedList<Packet>> {
+
+    let pathy = filename.path();
+    let rules = if let Ok(syntax_rules) = get_syntax_from_path(pathy){
+        syntax_rules
+    }
+    else {
+        return None;
+    };
+
+    let mut todos:LinkedList<Packet> = LinkedList::new();
+    
+
+    if let Ok(_) = matcher(filename, rules, &mut todos){
+        return Some(todos);
+    }
+    else {
+        return None;
+    }
+}
+
+
+pub fn iter_dir(path: String, pb: &ProgressBar) -> Result<(i32,i32, LinkedList<Packet>), Error> {
+    let files = if let Ok(open_file) = fs::read_dir(&path) {open_file} else {return Err(Error::new(ErrorKind::Other, format!("{} {}", "Could not read the directory:".red().bold(),path)));};
+
+    let mut todos:LinkedList<Packet> = LinkedList::new();
+    let mut filecount = 0;
+    let mut fileopenned = 0;
+    
+
+    for file in files {                                                     // for all entries in directory
+        if let Ok(ref this_file) = file {                                               // if this file is something and we have  permission to read it
+            if !this_file.path().is_dir() {                                                      // if this is a file then we try to get todos, if we fail then we continue the loop and increase filecount
+                pb.inc(this_file.metadata().expect("Cant open this files metadata: ").len());
+                filecount += 1;
+                if let Some(mut b) = get_todos(this_file) {
+                    fileopenned += 1;
+                    todos.append(&mut b);
+                }
+                else {
+                    continue;
+                }
+            }
+            else {
+                if let Ok((numc,numo,mut packs)) = iter_dir(this_file.path().into_os_string().into_string().unwrap(),&pb) {
+                    todos.append(&mut packs);
+                    filecount += numc;
+                    fileopenned += numo;
+                }
+                else {
+                    println!("{}{}","Could not open file/directory: ".red().bold(), this_file.file_name().as_os_str().to_str().unwrap());
+                    continue;
+                };
+                
+            }
+        }
+        else {
+            return Err(Error::new(ErrorKind::Other, "No directory entries."));
+        }
+    }
+    return Ok((filecount,fileopenned,todos));
 }
